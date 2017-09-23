a514ecdef600e0c6e9011b3aaff38927476fc8bd
Johan Almquist <johanalmquist@Johans-Air.lan>
2017-09-23 14:54:57 +0200

    Testing

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Book;
+use App\Mail\testMassMail;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 class WelcomeController extends Controller
 {
